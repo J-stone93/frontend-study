ecd401349fdfe9b1644d1da8f61409d7891576c6
J-stone93 <treous11@gmail.com>
2024-05-21 15:43:32 +0900

    24.05.21

--- 11_React/my-blog/src/App.js
+++ 11_React/my-blog/src/App.js
@@ -6,6 +6,7 @@ import MainPage from './component/page/MainPage';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import styled from 'styled-components';
 import PostViewPage from './component/page/PostViewPage';
+import PostWritePage from './component/page/PostWritePage';
 
 const MainTitleText = styled.p`
   font-size: 24px;
@@ -18,14 +19,21 @@ const MainTitleText = styled.p`
 function App() {
   return (
     <BrowserRouter>
-      <MainTitleText>미니 블로그</MainTitleText>
+      <MainTitleText>리액트 배워보기💻</MainTitleText>
       <Routes>
         <Route path='/' element={<MainPage />} />
+        <Route path='/post-write' element={<PostWritePage />} />
         <Route path='/post/:postId' element={<PostViewPage />} />
         {/* 여기서 :postId는 동적으로 변하는 파라미터를 위한 값 => URL 파라미터 */}
         {/* 경로에 콜론(:)을 쓰고 파라미터명을 입력하면
           연결된 컴포넌트에서 useParams() 훅을 사용해 postId 이름으로 해당 값을 가져올 수 있음
         */}
+
+        {/* (참고) */}
+        {/* URL 파라미터를 여러개 쓰고 싶다면 */}
+        {/* <Route path='/post/:postId/:otherValue' element={<PostViewPage />} /> */}
+        {/* 필수가 아닌 옵션값으로 처리하고 싶다면? Optional Segments */}
+        {/* <Route path='/post/:postId/:otherValue?' element={<PostViewPage />} /> */}
       </Routes>
     </BrowserRouter>
   );
