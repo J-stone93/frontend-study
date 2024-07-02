6189534c3e483c2ddd796436694ae50303a4dcb3
J-stone93 <treous11@gmail.com>
2024-07-02 15:00:49 +0900

    up

--- 11_React/my-app/src/chapter11/11.3/FileInput.jsx
+++ 11_React/my-app/src/chapter11/11.3/FileInput.jsx
@@ -1,4 +1,5 @@
 import { useRef } from "react";
+import axios from "axios";
 
 function FileInput() {
 
@@ -6,8 +7,31 @@ function FileInput() {
 
   const hadleSubmit = (e) => {
     e.preventDefault();
-    console.log(fileInput.current.files);
+    console.log(fileInput.current.files); // FileList 객체
     alert(`선택된 파일: ${fileInput.current.files[0].name}`)
+
+    const formData = new FormData();
+    // formData.append('title', title);
+    // formData.append('content', title);
+    formData.append('file',fileInput.current.files[0] || null);
+
+    // 아래 두 가지 방식 중에 택1
+    // 1) FormData()로 객체를 담아서 보내기
+    axios.post('https://api-url', formData);
+
+    // 2) 헤더에 콘텐트 타입 설정 시 자동으로 직렬화
+    axios.post('https://api-url', {
+      // title,
+      // content,
+      file: fileInput.current.files[0]
+    }, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+
+    });
+
+
   };
 
   // file input은 값을 사용자가 설정 할 수 없고 사용자가 첨부한 파일의 정보만 읽어올 수 있기 
